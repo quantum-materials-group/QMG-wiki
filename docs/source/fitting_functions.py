56b45d6b2598af9ec97c2c6fb8a7c1eac8419f06
quantum-materials-group <114585115+quantum-materials-group@users.noreply.github.com>
2022-09-29 00:08:36 +1000

    Update fitting_functions.py

--- docs/source/fitting_functions.py
+++ docs/source/fitting_functions.py
@@ -4,14 +4,14 @@ A collection of functions to fit to experimental data.
 
 __version__ = "0.1.0"
 
-def power_saturation(x, *p0):
+def power_saturation(x, p0):
     """
     A hyperbolic saturation function.
 
     :param x: 1D array of power values
     :type x: list[float] or np.array([float])
-    :param *p0: Parameters for hyperbolic function
-    :type *p0: list[I_inf, P_sat]
+    :param p0: Parameters for hyperbolic function
+    :type p0: list[I_inf, P_sat]
     :return: 1D array of intensity values
     :rtype: type(x)
     """
