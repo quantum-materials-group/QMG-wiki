fd02805d37bf8194ad02d7325d29193301b7036d
jjh-phd <114560561+jjh-phd@users.noreply.github.com>
2025-09-11 18:02:10 +1000

    Update plot_confocal_map.py

--- python-scripts/plot_confocal_map.py
+++ python-scripts/plot_confocal_map.py
@@ -9,8 +9,8 @@ files_path = load_files()
 
 #%%
 
-x_span = 100    # horizontal span (micron)
-y_span = 100    # vertical span (micron)
+spanX = 100    # horizontal span (micron)
+spanY = 100    # vertical span (micron)
 
 plt.figure(figsize=(1*3.5,1*2.675), dpi=600)
 plt.rcParams['font.size'] = '10'
@@ -19,7 +19,7 @@ cf_map = np.genfromtxt(files_path[0])
 cf_map = np.rot90(cf_map)
 cf_map /= np.max(cf_map)    # normalisation
 
-plt.imshow(cf_map, extent=(-x_span/2, x_span/2, -y_span/2, y_span/2))
+plt.imshow(cf_map, extent=(-spanX/2,spanX/2, -spanY/2, spanY/2))
 plt.xlabel(r'x ($\mu$m)')
 plt.ylabel(r'y ($\mu$m)')
 plt.colorbar(label='Intensity (arb. units)')
